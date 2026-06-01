042acd781ece94a908a8f728e92ff50b8510666d
Mateo Sierra <masierra29@poligran.edu.co>
2026-05-31 21:14:13 -0500

    Guardé cambios en clase 7

--- cursoDeFundamentosDePython/variables.py
+++ cursoDeFundamentosDePython/variables.py
@@ -6,3 +6,19 @@ print(x)
 
 X = "Esta es una variable diferente debido a la mayúscula"
 print(X)
+
+
+# VARIABLES VÁLIDAS
+mivariable = "Esta es una variable en minúscula"
+MIVARIABLE = "Esta es una variable en mayúscula"
+mi_variable = "Esta es una variable con guión bajos"
+camelCaseVariable = "Esta es una variable en camelCase"
+mivariable2 = "Esta es una variable con un número al final"
+PascalCaseVariable = "Se separa como camel pero inicia con mayúscula"
+
+
+# VARIABLES INVÁLIDAS
+
+2variable = "No se puede comenzar con un número"
+mi-variable = "No se pueden usar guiones en los nombres de variables"
+mi variable = "No se pueden usar espacios en los nombres de variables"
