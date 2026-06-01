08b613d2f1506aea5ad45bff5575c45f30d29536
Mateo Sierra <masierra29@poligran.edu.co>
2026-06-01 14:50:08 -0500

    Clase 9 y 10

---
+++ cursoDeFundamentosDePython/clase9-tipos-de-datos.py
@@ -0,0 +1,58 @@
+# TEXTO (String)
+"""
+comillasSimples = 'Este es un texto'
+comillasDobles = "Este es otro texto"
+comillasTriples = '''Comillas triples'''
+comillasTriples2 = Comillas triples también
+
+print(comillasSimples)
+print(comillasDobles)
+print(comillasTriples)
+print(comillasTriples2)
+"""
+
+
+# NÚMEROS
+"""
+entero = 42
+print(entero)
+
+flotante = 3.14
+print(flotante)
+
+imaginario = 2 + 3j
+print(imaginario)
+"""
+
+
+# Listas
+"""
+lista = [1, 2, 3, 4, 5]
+print(lista)
+"""
+
+# Tuplas
+"""
+tupla = ("a", "b", "c")
+print(tupla)
+"""
+
+#Diccionarios
+"""
+diccionario = {
+    "nombre": "Juan",
+    "edad": 30,
+    "ciudad": "Madrid"
+}
+print(diccionario)
+"""
+
+# Conjuntos (Sets)
+conjunto = {1, 1, 2, 2, 3}
+print(conjunto) #output: {1, 2, 3}
+
+
+# BOOLEANOS
+booleanoVerdadero = True
+booleanoFalso = False
+
