15fe5c6f64833fa6c1a6050b96e1a16b39e5ae49
Mateo Sierra <masierra29@poligran.edu.co>
2026-05-31 20:56:13 -0500

    clase 6 y 7

---
+++ cursoDeFundamentosDePython/Comentarios.py
@@ -0,0 +1,18 @@
+# Esta línea es un comentario de una sola línea
+print("Hola, mundo!")  # Este es un comentario al final de una línea de código
+
+# Este
+# es
+# un
+# comentario
+# de
+# varias
+# líneas    
+
+"""
+Este
+es
+otro
+comentario
+multilínea
+"""
---
+++ cursoDeFundamentosDePython/variables.py
@@ -0,0 +1,8 @@
+x = "Esta es una variable"
+print(x)
+
+x = "Ahora esta variable tiene un nuevo valor"
+print(x)
+
+X = "Esta es una variable diferente debido a la mayúscula"
+print(X)
