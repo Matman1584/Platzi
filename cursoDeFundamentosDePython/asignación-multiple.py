bf045af566304ce94f50d3e567f5f9127fce0fe4
Mateo Sierra <masierra29@poligran.edu.co>
2026-05-31 21:41:57 -0500

    Asignación-multiple.py clase8

---
+++ cursoDeFundamentosDePython/asignación-multiple.py
@@ -0,0 +1,16 @@
+x, y, z = "Manzana", "Naranja", "Banana"
+
+print(x)
+print(y)
+print(z)
+
+
+a = b = c = "Mandarina"
+print(a, b, c)
+
+print("Mi fruta favorita es la " + x)
+print(a + " " + z)
+
+d = 5
+e = 6
+print(d + e)
