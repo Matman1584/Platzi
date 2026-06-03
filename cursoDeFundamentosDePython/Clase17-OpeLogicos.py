b67ac1f201d3d5064543f2e5b7ed38137bcfc0a0
Mateo Sierra <masierra29@poligran.edu.co>
2026-06-03 14:29:27 -0500

    Clase 17 OpeLogicos

--- cursoDeFundamentosDePython/Clase17-OpeLogicos.py
+++ cursoDeFundamentosDePython/Clase17-OpeLogicos.py
@@ -1,6 +1,24 @@
 # OPERADORES DE COMPARACION
 
-X = 5
-Y = 3
+x = 5
+y = 3
+z = 5
 
-print(x == y) # 
+print(x == y) # ¿Sí es igual? True
+print(x != y) # Si x no es igual True
+print(x > y) # Si x es mayor True
+print(x < y) # si x es menor True
+print(x >= z) # Si x es mayor o igual True
+print(x <= y) # Si x es menor o igual True
+
+
+# UTILIZAR MÁS DE UN OPERADOR LÓGICO (AND, OR)
+x > y and y > z # Si ambas son verdaderas = True
+x > y or y > z # Si al menos una es verdadera = True
+
+
+# NEGACION
+v = True
+f = False
+print(not(v))
+print(not(f))
