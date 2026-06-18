44156cdb7db438782275b5c2d90997f4a2844290
Mateo Sierra <masierra29@poligran.edu.co>
2026-06-18 17:54:45 -0500

    Clase32

--- cursoDeFundamentosDePython/clase31-maquina-cafe/main.py
+++ cursoDeFundamentosDePython/clase31-maquina-cafe/main.py
@@ -1,4 +1,5 @@
 from menu import mostrar_menu
+from pedidos import pedir_cafe
 
 def main():
     while True:
@@ -6,7 +7,7 @@ def main():
         opcion = input("Selecciona una opcion: ")
 
         if opcion == "1":
-            # Pedir un cafe
+            pedir_cafe()
             pass
         elif opcion == "2":
             # Ver el historial
